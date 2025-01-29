4cf17f3b9abb1cc79dceb3a1fb50be19916d9f2c
Aman Deep <xamandeep360@gmail.com>
2025-01-29 15:04:51 +0530

    lowerBond.cpp
    This code only provide lower bond if the element you  want to search then it return nothing

--- lowerBond.cpp
+++ lowerBond.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
-using namespace std ;
-int main(){
-    int arr[]={2,4,6,8,10,12,14,16,18,20,22,24,26,29,33,55};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<"Number of the Element in the array is :== "<< n<<endl;
+using namespace std;
+int main()
+{
+    bool flag = false ;
+    int arr[] = {2,4,8,12,15,18,20,25,28,29,32,35,38};
+    int start = 0 ;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int last  = size-1;
+    int mid ;
     int target ;
-    cout<<"Enter the target Element :== " ;
-    cin>>target ; 
-    int first = 0 ;
-    int last = n - 1;
-    while(first <= last){
-        int mid  =  (first + last )/2 ; 
-        if(arr[mid]==target){
-            cout<<"Element found at the index of "<<last-1<<" And the position of "<<last<<" and the element is :== "<<arr[last-1];
-            break;
+    cout<<"Enter the target Element you want to print the lower bond :== ";
+    cin>> target ;
+    while(start<last)
+    {
+        mid = (start + last)/2 ;
+        if(arr[mid]==target)
+        {
+
+            flag = true ; 
+            break; 
         }
-        else if (arr[mid]>target){
-            last = mid - 1 ; 
+        if(arr[mid]<target)
+        {
+            start = mid + 1 ;
         }
-        else if (arr[mid]<target){
-            first = mid + 1 ;
+        if(arr[mid]>target)
+        {
+            last = mid -1;
         }
-    } 
+
+    }
+    if(flag == false)
+    {
+        cout<<"The lower bond is :== "<<arr[last]<<endl;
+    }
+
+
+
 }
